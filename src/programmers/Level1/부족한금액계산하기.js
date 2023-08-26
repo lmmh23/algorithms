bb1f682c872fe4192903d7740b4374925ee66e65
peter <peterlee11@naver.com>
2023-08-26 20:59:52 +0900

    23-08-26 프로그래머스 부족한 금액 계산하기 문제풀이

---
+++ src/programmers/Level1/부족한금액계산하기.js
@@ -0,0 +1,8 @@
+function solution(price, money, count) {
+  const map = Array.from({ length: count }, (_, idx) => idx + 1);
+  const acc = map.reduce((acc, cur) => acc += cur * price, 0);
+
+  return acc - money < 0 ? 0 : acc - money;
+}
+
+solution(3, 20, 4, 10);
