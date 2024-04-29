3434c75df429fcb43082254dcfa3a06fe14f0ef0
Lee Minhyung <106859397+1eeminhyung@users.noreply.github.com>
2024-04-29 10:25:38 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() 
+{
+	int x, y;
+	scanf("%d\n %d", &x, &y);
+	if (x > 0 && y > 0)
+		printf("1");
+	if (x < 0 && y > 0)
+		printf("2");
+	if (x < 0 && y < 0)
+		printf("3");
+	if (x > 0 && y < 0)
+		printf("4");
+}
