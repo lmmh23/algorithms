628cf35649eba68626b8b910335ea1333992badb
peter <peterlee11@naver.com>
2024-05-06 18:24:56 +0900

    feat : 폴더 형식 백준허브로 변경

--- src/programmers/Level1/내적.js
+++
@@ -1,5 +0,0 @@
-function solution(a, b) {
-  return a.reduce((acc, cur, idx) => acc += cur * b[idx], 0)
-}
-
-solution([1, 2, 3, 4], [-3, -1, 0, 2]);
--- src/programmers/Level1/부족한금액계산하기.js
+++
@@ -1,8 +0,0 @@
-function solution(price, money, count) {
-  const map = Array.from({ length: count }, (_, idx) => idx + 1);
-  const acc = map.reduce((acc, cur) => acc += cur * price, 0);
-
-  return acc - money < 0 ? 0 : acc - money;
-}
-
-solution(3, 20, 4, 10);
