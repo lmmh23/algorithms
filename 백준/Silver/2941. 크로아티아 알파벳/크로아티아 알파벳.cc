5d28f4d0fd3c259d6bbda89c73f63f75ff7473a1
1eeminhyeong <106859397+1eeminhyeong@users.noreply.github.com>
2024-10-03 18:20:21 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -1,48 +1,23 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
   string str;
   cin >> str;
+  int idx;
 
-  int cnt = 0;
-  int i = 0;
-
-  while (i < str.length()) {
-    if (i == str.length() - 1 && str[i] >= 'a' && str[i] <= 'z') {
-      cnt += 1;
-      i += 1;
-      continue;
-    }
-
-    if (str[i] == 'c' && (str[i + 1] == '=' || str[i + 1] == '-')) {
-      cnt += 1;
-      i += 2;
-      continue;
-    } else if (str[i] == 'd') {
-      if (str[i + 1] == '-') {
-        cnt += 1;
-        i += 2;
-        continue;
+  vector<string> croatian = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+  for (int i = 0; i < croatian.size(); i++) {
+    while (1) {
+      idx = str.find(croatian[i]);
+      if (idx == string::npos) {
+        break;
       }
-      if (i != str.length() - 2 && str[i + 1] == 'z' && str[i + 2] == '=') {
-        cnt += 1;
-        i += 3;
-        continue;
-      }
-    } else if ((str[i] == 'l' || str[i] == 'n') && str[i + 1] == 'j') {
-      cnt += 1;
-      i += 2;
-      continue;
-    } else if ((str[i] == 's' || str[i] == 'z') && str[i + 1] == '=') {
-      cnt += 1;
-      i += 2;
-      continue;
+      str.replace(idx, croatian[i].length(), "#");
     }
-    cnt += 1;
-    i++;
   }
-  cout << cnt;
+  cout << str.length();
 }
