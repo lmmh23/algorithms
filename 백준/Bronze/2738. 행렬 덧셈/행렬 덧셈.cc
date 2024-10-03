23ebb3d7fcef107290ca4e394da1bd3524472156
1eeminhyeong <106859397+1eeminhyeong@users.noreply.github.com>
2024-10-03 19:39:24 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,45 @@
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int N, M;
+
+  cin >> N >> M;
+
+  vector<vector<int>> A(N, vector<int>(M));
+  vector<vector<int>> B(N, vector<int>(M));
+
+  vector<vector<int>> result(N, vector<int>(M));  // 결과 행렬
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      cin >> A[i][j];
+    }
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      cin >> B[i][j];
+    }
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      result[i][j] = A[i][j] + B[i][j];
+    }
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      cout << result[i][j] << " ";
+    }
+    cout << endl;  // 각 행의 끝에서 줄바꿈
+  }
+
+  return 0;
+}
