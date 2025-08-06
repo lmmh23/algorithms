e19fc1bc599ff5419d9bc50c05fb575e162dab6f
lmmh23 <106859397+lmmh23@users.noreply.github.com>
2025-08-06 18:58:19 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -1,49 +1,23 @@
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
-int numToRank(int num) {
-    switch (num) {
-        case 6: return 1;
-        case 5: return 2;
-        case 4: return 3;
-        case 3: return 4;
-        case 2: return 5;
-        default: return 6;
-    }
-}
-
 vector<int> solution(vector<int> lottos, vector<int> win_nums) {
-    vector<int> answer;
+    int same = 0;
+    int zero = 0;
 
-    int numOfDirty = 0;
-    vector<int> filteredLottos;
-
-    for (int i: lottos) {
-        if (i == 0) {
-            numOfDirty++;
-        } else {
-            filteredLottos.push_back(i);
+    for (int lotto : lottos) {
+        if (find(win_nums.begin(), win_nums.end(), lotto) != win_nums.end()) {
+            same++;
+        }
+        if (lotto == 0) {
+            zero++;
         }
     }
 
-    int correct = 0;
-    for (int win: win_nums) {
-        for (int user: filteredLottos) {
-            if (win == user) {
-                correct++;
-                break; // 중복 방지
-            }
-        }
-    }
+    int best = (same + zero == 0) ? 6 : 7 - (same + zero);
+    int worst = (same == 0) ? 6 : 7 - same;
 
-    return {numToRank(correct + numOfDirty), numToRank(correct)};
-}
-
-
-int main() {
-    solution({44, 1, 0, 0, 31, 25}, {31, 10, 45, 1, 6, 19});
-    return 0;
+    return {best, worst};
 }
