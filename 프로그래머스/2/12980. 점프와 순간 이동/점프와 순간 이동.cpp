bc0cec3df89323efcc59d94480c0c6b357e90498
lmmh23 <106859397+lmmh23@users.noreply.github.com>
2025-08-27 14:28:10 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int ans = 0;
+    int dp[10] = {0};
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 2;
+    dp[4] = 1;
+    dp[5] = 2;
+
+    while (n > 5) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            ans += 1;
+            n = (n - 1) / 2;
+        }
+    }
+
+    ans += dp[n];
+    return ans;
+}
