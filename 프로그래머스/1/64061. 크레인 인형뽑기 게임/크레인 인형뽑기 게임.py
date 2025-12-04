630fe741ff1c4a02e5bc7d4de80a842c92574313
lmmh202 <106859397+lmmh202@users.noreply.github.com>
2025-12-04 21:18:22 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.07 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(board, moves):
+    answer = 0
+    
+    row = len(board)
+    basket = deque()
+    
+    for move in moves:
+        col = move - 1
+        
+        for row in range(len(board)):
+            if (board[row][col] != 0):
+                doll = board[row][col]
+                board[row][col] = 0
+                
+                if basket and basket[-1] == doll:
+                    basket.pop()
+                    answer += 2
+                else:
+                    basket.append(doll)
+                break
+    return answer
